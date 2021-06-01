ccd783b5c5b0a598104c2b837c397df8a6ac93c6
Veselin Georgiev <veselin.georgief@gmail.com>
2021-06-01 13:30:57 -0400

    post model changed

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken';
 
+const secret = 'test'
+
 const auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
@@ -8,9 +10,9 @@ const auth = async (req, res, next) => {
         let decodedData;
 
         if (token && isCustomAuth) {
-            decodedData = jwt.verify(token, 'test');
+            decodedData = jwt.verify(token, secret);
 
-            req.userId = decodedData?.id;
+            req.userId = decodedData?.id
         } else {
             decodedData = jwt.decode(token)
 
